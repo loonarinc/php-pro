87b544aea4b36768e054cf48daebe8a8b47657e8
loonarinc <loonarinc@gmail.com>
2019-09-20 14:15:00 +0300

    hw-3

---
+++ hw-3/models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\models;
+
+class Product extends Model
+{
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+
+    public function getTableName()
+    {
+        return 'products';
+    }
+
+
+
+
+}
